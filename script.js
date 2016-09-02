d9cc384d629bba2a8370d25fbf843d31dd887008
electromafr <pablo.larvor@gmail.com>
2016-09-02 15:55:32 +0200

    added modal video v1

--- script.js
+++ script.js
@@ -29,6 +29,9 @@ $(document).ready(function () {
         $(this).collapse('hide');
     }
   });
+
+  autoPlayYouTubeModal();
+
 });
 
 var cbpAnimatedHeader = (function() {
@@ -111,4 +114,24 @@ function onPlayerStateChange(event) {
 }
 function stopVideo() {
   player.stopVideo();
+}
+
+
+// Video in modal
+
+//FUNCTION TO GET AND AUTO PLAY YOUTUBE VIDEO FROM DATATAG
+function autoPlayYouTubeModal() {
+    var trigger = $("body").find('[data-toggle="modal"]');
+    trigger.click(function () {
+        var theModal = $(this).data("target"),
+            videoSRC = $(this).attr("data-theVideo"),
+            videoSRCauto = videoSRC + "?autoplay=1";
+        $(theModal + ' iframe').attr('src', videoSRCauto);
+        $(theModal + ' button.close').click(function () {
+            $(theModal + ' iframe').attr('src', videoSRC);
+        });
+        $('.modal').click(function () {
+            $(theModal + ' iframe').attr('src', videoSRC);
+        });
+    });
 }
